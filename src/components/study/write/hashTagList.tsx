047c37c163041758ce4d8f67a6a1e9ea3fffb2a9
hansejun <tpwnszja2@gmail.com>
2023-02-19 01:12:34 +0900

    Update hashTagList.tsx

--- src/components/study/write/hashTagList.tsx
+++ src/components/study/write/hashTagList.tsx
@@ -47,12 +47,12 @@ const HashTagList = ({
   return (
     <label className="grid grid-cols-[1fr_3fr] gap-[2.6rem]">
       <h3 className="Sub2 cursor-pointer hover:text-primary-500">{label}</h3>
-      <div className="rounded-[0.8rem] border border-primary-200 bg-[#F7F6F6]  text-[1.6rem]   focus:border-primary-400 flex">
-        <div className="flex space-x-[0.7rem] pl-[1rem] items-center">
+      <div className="rounded-[0.8rem] border border-primary-200 bg-[#F7F6F6]  text-[1.6rem]   focus:border-primary-400 flex space-x-[1.2rem]">
+        <div className="flex space-x-[0.7rem]  items-center">
           {hashTags?.map((tag, index) => (
             <div
               key={index}
-              className="flex items-center Sub2 textColor  rounded-full p-[1rem] bg-primary-main h-[4rem] space-x-[0.8rem] hover:bg-[#e46655] cursor-pointer"
+              className="flex items-center Sub2 textColor  rounded-full p-[1rem] bg-primary-main h-[4rem] space-x-[0.8rem] hover:bg-[#e46655] cursor-pointer first:ml-[1.4rem]"
               onClick={() => onDelete(index)}
             >
               <span className="text-white Sub2">#{tag}</span>
@@ -63,7 +63,7 @@ const HashTagList = ({
           ))}
         </div>
         <input
-          className="focus:outline-none  focus:ring-transparent border-none bg-transparent p-[1.4rem_1.8rem] text-[1.6rem] flex-1 placeholder:text-primary-400"
+          className="focus:outline-none  focus:ring-transparent border-none bg-transparent py-[1.8rem] text-[1.6rem] flex-1 placeholder:text-primary-400"
           type="text"
           placeholder={hashTags?.length ? "" : placeholder}
           onKeyDown={onKeyDown}
