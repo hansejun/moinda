51de169fdcd37758d7b3a4b6110ab7708a9c92ac
hansejun <tpwnszja2@gmail.com>
2023-03-06 09:37:48 +0900

    [bug] 채팅 버그 해결

--- src/components/myStudy/home/chats/chatList.tsx
+++ src/components/myStudy/home/chats/chatList.tsx
@@ -20,6 +20,7 @@ interface IProps {
 const ChatList = ({ roomId }: IProps) => {
   const scrollRef = useRef<Scrollbars>(null);
   const [chats, setChats] = useState<IChat[]>([]);
+  const [isMount, setIsMount] = useState(false);
   // 채팅 조회
 
   const getMessages = useCallback(() => {
@@ -49,39 +50,45 @@ const ChatList = ({ roomId }: IProps) => {
     }
   }, [chats]);
 
+  useEffect(() => {
+    setIsMount(true);
+  }, []);
+
   return (
     <div className=" = h-[45vh] max-h-[45vh] w-full flex-1 bg-[#F7F6F6]">
-      <Scrollbars autoHide ref={scrollRef}>
-        <ul className="flex w-full flex-col space-y-[1rem] px-[2.8rem] py-[1.8rem]">
-          {chats?.map((chat, idx) => (
-            <li className="flex  " key={chat.id}>
-              {chat?.avatarImg ? (
-                <Image
-                  className="aspect-square h-[2.2rem] w-[2.2rem] rounded-full"
-                  src={getImageUrl(chat.avatarImg)}
-                  width={22}
-                  height={22}
-                  alt="profile"
-                  priority
-                />
-              ) : (
-                <div
-                  className="flex-center Cap4 aspect-square w-[2.2rem] cursor-pointer 
+      {isMount && (
+        <Scrollbars autoHide ref={scrollRef}>
+          <ul className="flex w-full flex-col space-y-[1rem] px-[2.8rem] py-[1.8rem]">
+            {chats?.map((chat, idx) => (
+              <li className="flex  " key={chat.id}>
+                {chat?.avatarImg ? (
+                  <Image
+                    className="aspect-square h-[2.2rem] w-[2.2rem] rounded-full"
+                    src={getImageUrl(chat.avatarImg)}
+                    width={22}
+                    height={22}
+                    alt="profile"
+                    priority
+                  />
+                ) : (
+                  <div
+                    className="flex-center Cap4 aspect-square w-[2.2rem] cursor-pointer 
               rounded-full bg-[#9DA9B4]"
-                >
-                  {chat?.nickname.slice(0, 2)}
-                </div>
-              )}
-              <p className="mt-[0.2rem] ml-[1.6rem] mr-[0.8rem] whitespace-nowrap text-[1.2rem] font-medium text-primary-500">
-                {chat?.nickname}
-              </p>
-              <p className="mt-[0.2rem] break-all text-[1.2rem] font-medium">
-                {chat?.message}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </Scrollbars>
+                  >
+                    {chat?.nickname.slice(0, 2)}
+                  </div>
+                )}
+                <p className="mt-[0.2rem] ml-[1.6rem] mr-[0.8rem] whitespace-nowrap text-[1.2rem] font-medium text-primary-500">
+                  {chat?.nickname}
+                </p>
+                <p className="mt-[0.2rem] break-all text-[1.2rem] font-medium">
+                  {chat?.message}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </Scrollbars>
+      )}
     </div>
   );
 };
