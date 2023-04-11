6dd180996b6caaac7d9da0226770d19fbd437d5b
hansejun <tpwnszja2@gmail.com>
2023-04-11 21:17:21 +0900

    [feat] : studyList 페이지 skeleton UI

--- src/components/study/studyList/categoryStudyList.tsx
+++ src/components/study/studyList/categoryStudyList.tsx
@@ -1,6 +1,7 @@
 import studyApi from "@apis/query/studyApi";
 import { studyCategoryAtom } from "@atoms/studyAtom";
 import StudyCard from "@components/common/studyCard";
+import FakeStudyList from "@components/skeleton/studyList/FakeStudyList";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRecoilValue } from "recoil";
@@ -8,7 +9,7 @@ import { useRecoilValue } from "recoil";
 const CategoryStudyList: NextPage = () => {
   const category = useRecoilValue(studyCategoryAtom);
 
-  const { data: studyList } = studyApi.ReadNewStudyList({
+  const { data: studyList, isLoading } = studyApi.ReadNewStudyList({
     category: category || "TOTAL",
     count: 8,
     page: 1,
@@ -17,6 +18,7 @@ const CategoryStudyList: NextPage = () => {
   return (
     <div className="flex flex-col space-y-[1.8rem]">
       <h2 className="H2 text-primary-600">새로 생긴 스터디그룹</h2>
+      {isLoading && <FakeStudyList length={6} />}
       <div className="grid grid-cols-2 gap-[2.6rem] ">
         {studyList?.map((study) => (
           <Link href={`/study/${study.id}`} key={study.id} passHref>
--- src/components/study/studyList/recommendStudyList.tsx
+++ src/components/study/studyList/recommendStudyList.tsx
@@ -1,13 +1,14 @@
 import studyApi from "@apis/query/studyApi";
 import { studyCategoryAtom } from "@atoms/studyAtom";
 import StudyCard from "@components/common/studyCard";
+import FakeStudyList from "@components/skeleton/studyList/FakeStudyList";
 import Link from "next/link";
 import React from "react";
 import { useRecoilValue } from "recoil";
 
 const RecommendStudyList = () => {
   const category = useRecoilValue(studyCategoryAtom);
-  const { data: studyList } = studyApi.ReadRecommendStudyList({
+  const { data: studyList, isLoading } = studyApi.ReadRecommendStudyList({
     category: category,
     count: 3,
   });
@@ -15,6 +16,7 @@ const RecommendStudyList = () => {
   return (
     <div className="flex flex-col space-y-[1.8rem]">
       <h2 className="H2 text-primary-600">추천 스터디그룹</h2>
+      {isLoading && <FakeStudyList length={2} />}
       <div className="grid max-h-[29.4rem] grid-cols-[repeat(auto-fill,minmax(25rem,1fr))] gap-[2.6rem] overflow-hidden">
         {studyList?.map((study) => (
           <Link key={study?.id} href={`/study/${study?.id}`} passHref>
